b16f7bcb7664269ac0facced4831cbf49d37b4dc
Paweł Suliga <pawel.suliga@diag.pl>
2021-09-16 23:32:39 +0200

    a

---
+++ DynamicQuerying/Dictionaries/ComparisonType.cs
@@ -0,0 +1,15 @@
+namespace DynamicQuerying.Dictionaries
+{
+    public enum ComparisonType
+    {
+        Equal,
+        NotEqual,
+        GreaterThan,
+        LessThan,
+        GreaterOrEqual,
+        LessOrEqual,
+        Between,
+        StartWith,
+        Contains
+    }
+}
